daaceebe29cc2275b9e6a4623cc90f00d2d27d7b
bakatsuyuki <bakatsuyuki@icloud.com>
2020-03-07 13:35:23 +0900

    SecondFragmentをモジュール切り出し

--- second/src/main/java/com/bakatsuyuki/second/SecondFragment.kt
+++ second/src/main/java/com/bakatsuyuki/second/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.bakatsuyuki.android_practice
+package com.bakatsuyuki.second
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.bakatsuyuki.android_practice.databinding.FragmentSecondBinding
+import com.bakatsuyuki.second.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
